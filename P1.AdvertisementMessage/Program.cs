f13d354674ed7c21dc3ebfdb96b9bb20fd72c5bb
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2023-07-20 12:36:47 +0300

    Add project files.

---
+++ P1.AdvertisementMessage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P1.AdvertisementMessage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
