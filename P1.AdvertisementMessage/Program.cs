a1715c9ac64b806852787f0337fe015aab746260
I-M-Marinov <lcfrrr@gmail.com>
2023-07-24 01:54:29 -0500

    Added Solutions to P1.AdvertisementMessages and P2.Articles
    Added Solutions to P1.AdvertisementMessages and P2.Articles

--- P1.AdvertisementMessage/Program.cs
+++ P1.AdvertisementMessage/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace P1.AdvertisementMessage
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+
+        internal class Program
         {
-            Console.WriteLine("Hello World!");
+            static void Main(string[] args)
+            {
+                List<string> phrases = new List<string>() { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can't live without this product." };
+                List<string> events = new List<string>() { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+                List<string> authors = new List<string>() { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+                List<string> cities = new List<string>() { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+                Random random = new Random();
+
+                int numberOfRandomMessages = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < numberOfRandomMessages; i++)
+                {
+                    int indexPhrases = random.Next(phrases.Count);
+                    int indexEvents = random.Next(events.Count);
+                    int indexAuthors = random.Next(authors.Count);
+                    int indexCities = random.Next(cities.Count);
+
+                    Console.WriteLine($"{phrases[indexPhrases]} {events[indexEvents]} {authors[indexAuthors]} – {cities[indexCities]}.");
+                }
+            }
         }
     }
-}
+
+
+
--- P2.Articles/Program.cs
+++ P2.Articles/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace P2.Articles
 {
+
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> listArticles = Console.ReadLine().Split(", ").ToList();
+            Article article = new Article(listArticles[0], listArticles[1], listArticles[2]);
+
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < input; i++)
+            {
+                List<string> command = Console.ReadLine().Split(": ").ToList();
+
+                if (command[0] == "Edit")
+                {
+                    article.Content = command[1];
+                }
+                else if (command[0] == "ChangeAuthor")
+                {
+                    article.Author = command[1];
+                }
+                else if (command[0] == "Rename")
+                {
+                    article.Title = command[1];
+                }
+            }
+            Console.WriteLine($"{article.Title} - {article.Content}: {article.Author}");
         }
     }
 }
