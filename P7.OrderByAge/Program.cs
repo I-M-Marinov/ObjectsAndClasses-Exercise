db86a77e899e242cc97e8558e16b32ba3e65be92
I-M-Marinov <lcfrrr@gmail.com>
2023-07-25 09:29:37 -0500

    Added Solution to P7.OrderByAge
    Added Solution to P7.OrderByAge

---
+++ P7.OrderByAge/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P7.OrderByAge
+{
+    public class People
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public int Age { get; set; }
+
+        public People(string name, string id, int age)
+        {
+            Name = name;
+            ID = id;
+            Age = age;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split(" ").ToList();
+            List<People> people = new List<People>();
+
+            while (input[0] != "End")
+            {
+                string name = input[0];
+                string id = input[1];
+                int age = int.Parse(input[2]);
+                People person = new People(name, id, age);
+                people.Add(person);
+
+                input = Console.ReadLine().Split(" ").ToList();
+            }
+            foreach (People person in people.OrderBy(x => x.Age))
+            {
+                Console.WriteLine($"{person.Name} with ID: {person.ID} is {person.Age} years old.");
+            }
+        }
+    }
+}
